62dfb1be8e3eea26bdd193920be97f0d6b792787
Darren Atienza <datienza@doehle-mnl.com>
2022-12-09 18:52:59 +0800

    routing

---
+++ AnalyticsConsumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "127.0.0.1" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message Recieved: {message}");
+};
+
+channel.BasicConsume(queue: "letterbox", autoAck: true, consumer: consumer);
+
+Console.ReadKey();
