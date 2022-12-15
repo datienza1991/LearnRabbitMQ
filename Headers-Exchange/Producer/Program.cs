a87bcabf2025812ae0e9ecc645ad9210efdaa5e6
Darren Atienza <datienza@doehle-mnl.com>
2022-12-15 14:48:03 +0800

    header exchange

---
+++ Headers-Exchange/Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "127.0.0.1" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "headersexchange", type: ExchangeType.Headers);
+
+var message = "This message will be sent with headers";
+
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+var properties = channel.CreateBasicProperties();
+properties.Headers = new Dictionary<string, object>{
+    {"name", "brian"}
+};
+
+channel.BasicPublish("headersexchange", "", properties, encodedMessage);
+
+Console.WriteLine($"Published message: {message}");
